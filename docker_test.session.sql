88be514040a9ef425e3235d6ab0293da779a076b
freshcodeAlice <freshjs.ofte@gmail.com>
2024-04-05 12:01:01 +0300

    docker commands

---
+++ docker_test.session.sql
@@ -0,0 +1,14 @@
+CREATE TABLE cats(
+    name varchar(200),
+    age int,
+    color varchar(200)
+);
+
+INSERT INTO cats VALUES 
+('Murzik', 5, 'white'), ('Kytsia', 2, 'grey'), ('Barsik', 4, 'black');
+
+SELECT * FROM cats;
+
+
+INSERT INTO cats VALUES 
+('Kitty', 1, 'pink');
