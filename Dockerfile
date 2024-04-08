5b5097da1299ae767e61dee19ace5644bbb2f3db
freshcodeAlice <freshjs.ofte@gmail.com>
2024-04-08 10:13:06 +0300

    Dockerfile containerization

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18-alpine
+
+RUN mkdir /app
+
+WORKDIR /app
+
+COPY ./ ./
+
+RUN npm i
+
+EXPOSE 3000
+
+CMD ["npm", "run", "start"]
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use((req, res, next) => {
-    res.status(200).send('Hello to you!')
+    res.status(200).send('Another response inside container')
 })
 
 const server = http.createServer(app);
